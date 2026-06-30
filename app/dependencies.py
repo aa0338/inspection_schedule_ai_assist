7f9438d6ead4261c6c58927e71ae0ac81ceb8578
Jae-Sang <aa0338@naver.com>
2026-06-30 16:12:47 +0900

    임포트오류

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 from fastapi import Request, Depends
-from reinspection_risk_prediction.rrp_service import RrpService
+from app.reinspection_risk_prediction.rrp_service import RrpService
 from optimal_period_advisor.opa_service import OpaService
-from model_manager import ModelManager
+from app.model_manager import ModelManager
 
 def get_model_manager(request: Request) -> ModelManager:
     return request.app.state.model_manager
