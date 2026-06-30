3aea13c86812c1616de43cc7a440ba206938f8e5
Jae-Sang <aa0338@naver.com>
2026-06-30 15:42:49 +0900

    오타

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     RRP_MODEL_PATH: str
     OPA_MODEL_PATH: str
     
-    model_config = SettingConfigDict(
+    model_config = SettingsConfigDict(
         env_file=(".env", f"env.{env_state}", "env.local"),
         env_file_encoding="utf-8",
         extra="ignore" # 정의되지 않은 변수는 무시. "allow"는 미리 정의된 변수 이외에도 허용
